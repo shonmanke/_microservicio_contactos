ac60cea4eb0a6f82c9a7e3fb2d3f3459b7c1e5c8
unknown <sjonathanarias@gmail.com>
2022-08-05 22:41:01 -0300

    subiendo proyecto

---
+++ src/main/java/dao/AgendaJpaSpring.java
@@ -0,0 +1,18 @@
+package dao;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import model.Contacto;
+
+public interface AgendaJpaSpring extends JpaRepository<Contacto, Integer> {
+	Contacto findByEmail (String email);
+	@Transactional
+	@Modifying
+	@Query("Delete from Contacto c Where c.email=?1")
+	void eliminarPorEmail(String email); 
+
+}
